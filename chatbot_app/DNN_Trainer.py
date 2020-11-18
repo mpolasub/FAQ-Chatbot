7191cdd8fdd99063553a4555e87b1cba1cd0740b
ML Club <bhsml2020@gmail.com>
2020-11-17 18:53:37 -0800

    better model with reLU

--- chatbot_app/DNN_Trainer.py
+++ chatbot_app/DNN_Trainer.py
@@ -85,9 +85,8 @@ output = numpy.array(output)
 ops.reset_default_graph()
 
 net = tflearn.input_data(shape=[None, len(training[0])])
-net = tflearn.fully_connected(net, 8, activation=activator)
-net = tflearn.fully_connected(net, 8, activation=activator)
-net = tflearn.fully_connected(net, 8, activation=activator)
+net = tflearn.fully_connected(net, 32, activation=activator)
+net = tflearn.fully_connected(net, 32, activation=activator)
 net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
 net = tflearn.regression(net)
 
